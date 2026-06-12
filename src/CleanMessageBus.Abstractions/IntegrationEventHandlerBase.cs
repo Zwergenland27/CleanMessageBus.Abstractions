4106ea36959f4f035f6a830921c16a81b650974b
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2026-06-12 21:41:54 +0200

    chore: Remove deprecated methods from IntegrationEventHandlerBase

--- src/CleanMessageBus.Abstractions/IntegrationEventHandlerBase.cs
+++ src/CleanMessageBus.Abstractions/IntegrationEventHandlerBase.cs
@@ -9,37 +9,6 @@ public abstract class IntegrationEventHandlerBase<TIntegrationEvent> : IRequestH
     where TIntegrationEvent : IntegrationEvent
 {
     /// <summary>
-    /// The max. amount of events that can be polled from the broker to the active instance simultaneously
-    /// </summary>
-    public uint MaxQueueSize { get; private init; }
-    
-    /// <summary>
-    /// Name of the producer
-    /// </summary>
-    public string ProducerName { get; private init; }
-    
-    /// <summary>
-    /// Initializes the handler with a specific queue size
-    /// </summary>
-    /// <param name="producerName">Name of the producer</param>
-    /// <param name="maxQueueSize">The max. amount of events that can be polled from the broker to the active instance simultaneously</param>
-    protected IntegrationEventHandlerBase(string producerName, uint maxQueueSize)
-    {
-        ProducerName = producerName;
-        MaxQueueSize = maxQueueSize;
-    }
-    
-    /// <summary>
-    /// Initializes the handler without a specific queue size
-    /// </summary>
-    /// <param name="producerName">Name of the producer</param>
-    protected IntegrationEventHandlerBase(string producerName)
-    {
-        ProducerName = producerName;
-        MaxQueueSize = uint.MaxValue;
-    }
-    
-    /// <summary>
     /// Actual event handler logic
     /// </summary>
     /// <param name="event">Incoming event object</param>
